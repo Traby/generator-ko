e106dfccc489dd9f881d7ab83d967b30aa123395
Karl Ostendorf <karl@ostendorf.com>
2013-08-15 13:52:41 +0200

    eliminated mock from unit test

--- app/templates/test/unit/controllers/home.spec.js
+++ app/templates/test/unit/controllers/home.spec.js
@@ -2,34 +2,31 @@ describe('home controller', function () {
 
     'use strict';
 
-    var scope, controller;
-
-    var cfgMock = {
-        getAppName: function () {
-            return 'blah blah';
-        }
-    };
+    var scope, controller, cfg;
 
     beforeEach(function () {
-
         module('app');
+    });
 
-        spyOn(cfgMock, 'getAppName').andCallThrough();
+    beforeEach(inject(function ($injector) {
+        cfg = $injector.get('configuration');
+        spyOn(cfg, 'getAppName').andCallThrough();
+    }));
 
+    beforeEach(function () {
         inject(function ($rootScope, $controller) {
             scope = $rootScope.$new();
             controller = $controller('HomeCtrl', {
                 $scope: scope,
-                configuration: cfgMock
+                configuration: cfg
             });
         });
-
     });
 
     it('should be defined', function () {
         expect(scope.title).toBeDefined();
-        expect(scope.title).toBe('blah blah');
-        expect(cfgMock.getAppName).toHaveBeenCalled();
+        expect(scope.title).toBe('My App');
+        expect(cfg.getAppName).toHaveBeenCalled();
     });
 
 });
