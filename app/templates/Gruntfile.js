cbb9edb46ffc45214665426ccc7b4fb0d66d9d8d
Karl Ostendorf <karl@ostendorf.com>
2013-10-10 10:37:33 +0200

    added grunt server command

--- app/templates/Gruntfile.js
+++ app/templates/Gruntfile.js
@@ -2,14 +2,6 @@ module.exports = function (grunt) {
 
     'use strict';
 
-	grunt.loadTasks('scripts/tasks');
-    grunt.loadNpmTasks('grunt-contrib-jshint');
-    grunt.loadNpmTasks('grunt-karma');
-    grunt.loadNpmTasks('grunt-recess');
-
-    grunt.registerTask('default', ['test', 'jshint']);
-    grunt.registerTask('build', ['recess']);
-
     grunt.initConfig({
 
         jshint: {
@@ -90,7 +82,42 @@ module.exports = function (grunt) {
                     zeroUnits: false
                 }
             }
-        }
+        },
+
+		express: {
+			options: {
+				background: false,
+				port: 8000,
+				script: 'scripts/server.js'
+			},
+			test: {
+				args: [ ]
+			}
+		}
+
+    });
+
+	grunt.loadTasks('scripts/tasks');
+    grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-express-server');
+    grunt.loadNpmTasks('grunt-karma');
+    grunt.loadNpmTasks('grunt-recess');
+
+    grunt.registerTask('default', ['test', 'jshint']);
+    grunt.registerTask('build', ['recess']);
+
+    // local server
+    grunt.registerTask('server', 'run local server', function() {
+
+		if (!this.flags.run) {
+			var open = require('open');
+			grunt.log.writeln('opening http://localhost:8000/ ...');
+			setTimeout(function() {
+				open('http://localhost:8000/');
+			}, 1000);
+		}
+
+		grunt.task.run('express:test');
 
     });
 
